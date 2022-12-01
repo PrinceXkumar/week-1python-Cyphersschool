5921fdf9842a678a16f7e52f87b738f75a79d20f
PrinceXkumar <118840584+PrinceXkumar@users.noreply.github.com>
2022-12-01 22:48:24 +0530

    Add files via upload

---
+++ class10-escape sequences-CipherSchools.py.py
@@ -0,0 +1,3 @@
+print(' I\'m Prince Kumar ')
+print('Be it \\ dfg')
+print("git bash \"")
---
+++ class11-comments in python-CipherSchools.py.py
@@ -0,0 +1,3 @@
+print(' I\'m Prince Kumar ') # \' is for '
+print('Be it \\ dfg') # \\ is for \
+print("git bash \"") # \" to print "
